202e071b63c9e586dd2e1aec6f45fc2d64553997
ONKJUNUA <jaseko2002@gmail.com>
2022-06-18 22:29:57 +0200

    setting up

---
+++ code.py
@@ -0,0 +1,29 @@
+import PySimpleGUI as sg
+
+sg.theme('reddit')
+
+image_col = sg.Column([[sg.Image(key = '-IMAGE-',background_color = '#FFFFFF')]])
+info_col = sg.Column([
+    [sg.Text('', key = '-LOCATION-', font = 'Calibri 30', background_color = '#FF0000', pad = 0, visible = False)],
+    [sg.Text('', key = '-TIME-', font = 'Calibri 16', background_color = '#000000', text_color = '#FFFFFF', pad = 0, visible = False)],
+    [sg.Text('', key = '-TEMP-', font = 'Calibri 16', pad = (0,10), background_color = '#FFFFFF', text_color = '#000000', justification = 'center', visible = False)]
+])
+layout = [
+    [sg.Input(expand_x = True, key = '-INPUT-'),sg.Button('Enter')],
+    [image_col,info_col]
+    ]
+
+window = sg.Window('WeatherApp', layout)
+
+while True:
+    event, values = window.read()
+    if event == sg.WIN_CLOSED:
+        break
+
+    if event == 'Enter':
+        window['-LOCATION-'].update('POLAND, CRACOW',visible = True)
+        window['-TIME-'].update('00:00',visible = True)
+        window['-TEMP-'].update('20`C',visible = True)
+        window['-IMAGE-'].update('assets/snow.png')
+
+window.close()
